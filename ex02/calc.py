8ee33fb9b86ef4959b8d3b7bde93bd428c9e6a50
Kato Yui <c0b2104388@edu.teu.ac.jp>
2022-10-04 15:12:18 +0900

    練習6まで

--- ex02/calc.py
+++ ex02/calc.py
@@ -3,17 +3,19 @@ import tkinter.messagebox as tkm
 
 root = tk.Tk()
 root.title("電卓")
-root.geometry("300x500")#練習1
+root.geometry("300x600")#練習1
 
 def button_crick(event):
     button = event.widget
-    num = int(button["text"])
+    num = button["text"]
     #tkm.showinfo(f"{num}", f"{num}のボタンが押されました")#練習3
     entry.insert(tk.END, num)#練習5
 
 takasa = 1
 yoko = 0
-for i, num in enumerate(range(9, -1, -1), 1):
+numbers = list(range(9, -1, -1))
+hugo = ["+"]
+for i, num in enumerate(numbers+hugo, 1):
     button = tk.Button(root, text = f"{num}", font=("Times New Roman", 30), width=4, height=2)
     button.bind("<1>", button_crick)
     button.grid(row = takasa, column = yoko)
@@ -21,6 +23,9 @@ for i, num in enumerate(range(9, -1, -1), 1):
     if i % 3 == 0:
         takasa += 1
         yoko = 0 #練習2
+
+#plus_button = tk.Button(root, text="+", font=("", 30), #width=4, height = 2)
+#plus_button.grid(row = 4, column = 1)
 
 entry = tk.Entry(root, justify="right", width=10, font=("Times New Roman", 40))
 entry.grid(row = 0, column =0, columnspan = 3)#練習4
