281fc9c3dc807e5f66d90f59492c6a7ac995fc89
Kato Yui <c0b2104388@edu.teu.ac.jp>
2022-10-04 14:35:16 +0900

    練習3まで

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,15 +5,22 @@ root = tk.Tk()
 root.title("電卓")
 root.geometry("300x500")#練習1
 
+def button_crick(event):
+    button = event.widget
+    num = int(button["text"])
+    tkm.showinfo(f"{num}", f"{num}のボタンが押されました")#練習3
+
 r = 0
 c = 0
 for i, num in enumerate(range(9, -1, -1), 1):
     button = tk.Button(root, text = f"{num}", font=("Times New Roman", 30), width=4, height=2)
-
+    button.bind("<1>", button_crick)
     button.grid(row = r, column = c)
     c += 1
     if i%3==0:
         r += 1
-        c = 0
+        c = 0 #練習2
+
+
 
 root.mainloop()
