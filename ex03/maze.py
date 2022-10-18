94ccc279bffce3f0387b48a889f99946473ce8b2
Kato Yui <c0b2104388@edu.teu.ac.jp>
2022-10-18 14:28:21 +0900

    練習５まで

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,7 +1,24 @@
 import tkinter as tk
 
+def key_down(event):
+    global key
+    key = event.keysym
+    print(key)
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
+
+    canv = tk.Canvas(root, width=1500, height=900, bg="black")
+    canv.pack()
+
+    tori = tk.PhotoImage(file="fig/5.png")
+    cx, cy = 300, 400
+    canv.create_image(cx, cy, image=tori, tag="tori")
+
+    key = ""
+
+    root.bind("<KeyPress>", key_down)
 
     root.mainloop()
