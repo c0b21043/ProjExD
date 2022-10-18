6b6ab93e5935f1a66b1ae219bddb27632e3ceccb
Kato Yui <c0b2104388@edu.teu.ac.jp>
2022-10-18 17:11:48 +0900

    4限おわり！

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,9 @@
+import imghdr
 import tkinter as tk
+from typing import TYPE_CHECKING
 import maze_maker as mm
+import tkinter.messagebox as tkm
+
 
 def key_down(event):
     global key
@@ -9,6 +13,15 @@ def key_down(event):
 def key_up(event):
     global key
     key = ""
+
+
+#ボタンクリックによってindexを増やし、古い画像を消して新しい画像を作り直している
+def btn_click(event):
+    global index
+    index = (index+1) % len(photos)
+    canv.delete("tori")
+    canv.create_image(cx, cy, image=photos[index], tag="tori")
+    tkm.showinfo("画像変更", "こうかとんの画像が変更されました！\nかわいいですね^^")
 
 
 def main_proc():
@@ -38,6 +51,8 @@ def main_proc():
     canv.coords("tori", cx, cy)
     root.after(100, main_proc)
 
+index = 0 #画像のindexをグローバルで管理する
+
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -49,15 +64,29 @@ if __name__ == "__main__":
     maze_lst = mm.make_maze(15, 9) #1：壁、0：床
     mm.show_maze(canv, maze_lst)
 
-    tori = tk.PhotoImage(file="fig/1.png")
+    #こうかとんの写真のリスト
+    photos=[tk.PhotoImage(file='fig/1.png'),
+            tk.PhotoImage(file='fig/2.png'),
+            tk.PhotoImage(file='fig/3.png'),
+            tk.PhotoImage(file='fig/4.png'),
+            tk.PhotoImage(file='fig/5.png'),
+            tk.PhotoImage(file='fig/6.png'),
+            tk.PhotoImage(file='fig/7.png'),
+            tk.PhotoImage(file='fig/8.png'),
+            tk.PhotoImage(file='fig/9.png'),]
+
     mx, my = 1, 1
     cx, cy = mx*100+50, my*100+50
-    canv.create_image(cx, cy, image=tori, tag="tori")
+    canv.create_image(cx, cy, image=photos[index], tag="tori")
 
     key = ""
 
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
+
+    btn = tk.Button(root, text="好きなこうかとんを選ぼう！", font=("", 20), width=50, height=50)
+    btn.pack(ipadx=10, ipady=5)
+    btn.bind("<1>", btn_click)
 
     main_proc()
 
