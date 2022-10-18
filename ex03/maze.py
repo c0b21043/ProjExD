267265294762fdea49c14d5cf5311d6b0d5da962
Kato Yui <c0b2104388@edu.teu.ac.jp>
2022-10-18 15:20:11 +0900

    練習１１まで

--- ex03/maze.py
+++ ex03/maze.py
@@ -12,16 +12,17 @@ def key_up(event):
 
 
 def main_proc():
+    global mx, my
     global cx, cy
     if key == "Up":
-        cy -= 20
+        my -= 1
     if key == "Down":
-        cy += 20
+        my += 1
     if key == "Left":
-        cx -= 20
+        mx -= 1
     if key == "Right":
-        cx += 20
-
+        mx += 1
+    cx, cy = mx*100+50, my*100+50
     canv.coords("tori", cx, cy)
     root.after(100, main_proc)
 
@@ -32,9 +33,14 @@ if __name__ == "__main__":
 
     canv = tk.Canvas(root, width=1500, height=900, bg="black")
     canv.pack()
+    
+    maze_lst = mm.make_maze(15, 9)
+    #1：壁、0：床
+    mm.show_maze(canv, maze_lst)
 
     tori = tk.PhotoImage(file="fig/1.png")
-    cx, cy = 300, 400
+    mx, my = 1, 1
+    cx, cy = mx*100+50, my*100+50
     canv.create_image(cx, cy, image=tori, tag="tori")
 
     key = ""
@@ -43,9 +49,5 @@ if __name__ == "__main__":
     root.bind("<KeyRelease>", key_up)
 
     main_proc()
-
-    maze_lst = mm.make_maze(15, 9)
-    #print(maze_lst) #1：壁、0：床
-    mm.show_maze(canv, maze_lst)
 
     root.mainloop()
